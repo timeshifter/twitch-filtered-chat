9cd2ccf3b18a4d87665b33b3cd6fd17f5a65349b
Peter Schultz <kaedenn@gmail.com>
2018-11-19 15:52:47 -0900

    Separated things into more files; added reference to my twitch-api repo

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Twitch Filtered Chat
+// Primary Script
 
 /* Hijacking fixes:
  *
@@ -10,28 +12,6 @@
 /* TODO: markdown
  * https://github.com/showdownjs/showdown
  */
-
-var default_colors = [
-    '#0000FF',
-    '#008000',
-    '#FF0000',
-    '#B22222',
-    '#FF7F50',
-    '#9ACD32',
-    '#FF4500',
-    '#2E8B57',
-    '#DAA520',
-    '#D2691E',
-    '#5F9EA0',
-    '#1E90FF',
-    '#FF69B4',
-    '#8A2BE2',
-    '#00FF7F'
-];
-
-function GetRandomColor() {
-    return default_colors[Math.floor(Math.random() * default_colors.length)];
-}
 
 var EscapeCharsList = ['&', '"', "'", '<', '>'];
 var EscapeChars = {
@@ -65,32 +45,11 @@ var _emoteReq,
     cheerLevels = [],
     valid_cheers = [];
 
-/* Users allowed to use "force" */
-var super_users = {
-    Kaedenn_: 1,
-    MediaMagnet: 1,
-    dwangoAC: 1
-};
-
+// Configuration object
 var config = {
     Channel: '',
     Nick: '',
     Pass: ''
-};
-
-/* Known style keywords and their cost */
-var valid_styles = {
-    marquee: {cost: 1, value: ['<marquee>', '</marquee>']},
-    bold: {cost: 1, value: ['<b>', '</b>']},
-    italic: {cost: 1, value: ['<i>', '</i>']},
-    underline: {cost: 1, value: ['<span style="text-decoration: underline;">', '</span>']},
-    upsidedown: {cost: 1, value: ['<span style="display:block; transform: rotate(180deg); text-align: right;">', '</span>']},
-    inverted: {cost: 1, value: ['<span style="filter: invert(100%);">', '</span>']},
-    strikethrough: {cost: 1, value: ['<span style="text-decoration: line-through;">', '</span>']},
-    subscript: {cost: 1, value: ['<sub>', '</sub>']},
-    superscript: {cost: 1, value: ['<sup>', '</sup>']},
-    big: {cost: 1, value: ['<span style="font-size: larger">', '</span>']},
-    small: {cost: 1, value: ['<span style="font-size: smaller">', '</span>']}
 };
 
 (function () {
